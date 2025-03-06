a2fe063d706db46cba51ca70040606d111de835a
seilleskz <seilleskz@gmx.com>
2025-03-06 17:12:44 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solve(arr));
+    }
+
+    public static long solve(int[] arr) {
+        // write your code here
+        return -1;
+    }
+}
